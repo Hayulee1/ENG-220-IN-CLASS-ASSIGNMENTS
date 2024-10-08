7eee144d5b615f1df6a3ca11987727e1d3e00a78
Hayulee1 <135177814+Hayulee1@users.noreply.github.com>
2024-10-08 14:45:33 -0600

    Update untitled8(2).py
    indention

--- untitled8(2).py
+++ untitled8(2).py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1Z1PK7gmTHuHbgUykWJcFanDl2D1CaLCY
 """
 
-import streamlit as st
+    import streamlit as st
 
     import pandas as pd
     # Title of the app
